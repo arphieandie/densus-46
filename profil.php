63348f86bef963341ab6336f886e6820195db59c
tonggor <ktonggor@yahoo.com>
2013-12-09 10:30:14 +0700

    ini punyaku pung

--- profil.php
+++ profil.php
@@ -4,11 +4,61 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Newbie</title>
 <link rel="stylesheet" href="tampilan.css" type="text/css" />
+<style type="text/css">
+.bandana{
+	background:rgba(0,0,0,0.8);
+	color:#0FF;
+	width:94%;
+	height:30px;
+	padding:10px;
+	font-family:Verdana, Geneva, sans-serif;
+	font-size:14px;
+	margin-bottom:10px;
+	border-radius:8px;
+}
+.bandana:hover{
+	background:url(images/header.png);
+	color:#030;
+}
+.gambarphoto{
+	border:0;
+	border-radius:8px;
+	width:200px;
+	height:200px;
+	margin:5px;
+}
+.donloadlagu{
+	color:#3FF;
+	font-family:Verdana, Geneva, sans-serif;
+	font-size:14px;
+	font-style:italic;
+	float:left;
+	width:50%;
+	background:rgba(0,0,0,0.8);
+	/*border-radius:4px;*/
+	border-bottom:#9CF;
+	border-bottom-style:solid;
+	padding:10px;
+}
+.doload{
+	float:right;
+}
+.download{
+	width:200px;
+	height:30px;
+	float:right;
+	background:#3F3;
+	border-radius:6px;
+}
+.download:hover{
+	background:#9F6;
+}
+</style>
 </head>
 <body>
 	<div class="header">
     	<div class="section">
-			<a href="index.php"><img class="gbr" src="images/newbielogo copy.jpg" width="160px" height="80px" alt="Image" /></a>
+			<a href="index.php"><img class="gbr" src="images/newbiehead.jpg" width="160px" height="80px" alt="Image" /></a>
 			<ul>
 				<li>
 					<a href="index.php">Home</a>
@@ -25,24 +75,67 @@
     <div class="band">
     	<div class="daftarband">
 			<div class="judulin"><h2>Band</h2></div>
+            <div class="jarak"></div>
+            <div class="jarak"></div>
+            <?php
+				include "db.php";
+				$query = "SELECT * FROM datamember";
+				$hasil_query = mysql_query($query);
+		
+				while ($status=mysql_fetch_row($hasil_query)) {
+					echo "<a onclick=''><div class='bandana'><img src='images/sound.jpg' width='30px' height='30px' align='left'/>$status[0]</div></a>";
+				}
+			?>
             
         </div>
         <div class="status">
-        	<div class="judulin"><h2>Status</h2></div>
-            
+        	<div class="judulin"><h2>Nama Band</h2></div>
+            <div class="jarak1"></div>
+                <!--<div id="komen">
+					<?php
+                        /*mysql_connect("localhost","root","");
+                        mysql_select_db("newbie");
+                        $query = "SELECT * FROM status ORDER BY Date DESC";
+                        $hasil_query = mysql_query($query);
+                
+                        while ($status=mysql_fetch_row($hasil_query)) {
+                            echo "<div class='tampilkomen'><h3 class='nama'>$status[1] - $status[0]</h3></div>";
+                        }*/
+                    ?>
+                </div>-->
         </div>
         <div class="lagu">
 			<div class="judulin"><h2>Lagu</h2></div>
+            <div class="jarak"></div>
+            <div class="jarak"></div>
+            <?php
+			/*$member=$_SESSION["Login"];username,='".$member."'*/    
+			$q1 = "SELECT namalagu,lokasi from lagu"; //memilih file dari database
+			$result = mysql_query($q1);
+			while ($data = mysql_fetch_array($result)) {
+				$loc = $data['lokasi'];
+				echo"<div class='donloadlagu'><img src='images/sound.jpg' width='20px' height='20px' align='left'/>$data[0]
+						<form class='doload'>
+						<input type=\"button\" value=\"Download\" class=\"download\" onclick=\"window.location.href='transaksi.php'\" />
+            			</form>
+						</div>";
+				}
+			?> 
         </div>
         <div class="photo">
         	<div class="judulin"><h2>Photo</h2></div>
-            <!--<script type="text/javascript" src="simpleviewer.js"></script>
-			<script type="text/javascript">
-				simpleviewer.ready(function () {
-				simpleviewer.load('sv-container', '100%', '100%', '222222', true);
-				});
-			</script>
-			<div id="sv-container"></div>-->
+            <div class="jarak"></div>
+            <div class="jarak"></div>
+             <?php
+			/*$member=$_SESSION["Login"];username,='".$member."'*/    
+			$q1 = "SELECT namafoto,lokasi from photo"; //memilih file dari database
+			$result = mysql_query($q1);
+			while ($data = mysql_fetch_array($result)) {
+				$loc = $data['lokasi'];
+				echo"<img class='gambarphoto' src=$loc align='left'/>";
+				}
+			?>       
+
         </div>
     </div>
     <div class="footer">
