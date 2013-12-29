127449a5659856765bed67f884e06396188cf5d9
tonggor <ktonggor@yahoo.com>
2013-12-29 14:29:34 +0700

    Update Proses_Register.php

--- Proses_Register.php
+++ Proses_Register.php
@@ -9,22 +9,26 @@
 	$password=md5($pass);
 	$repass=md5($re);
 	
-	if(empty($username)){
+	if(empty($user)){
 		echo "<script type='text/javascript'>alert ('USERNAME KOSONG !! Silahkan Masukkan Username Dengan Benar');</script>";
 		include "index.php";
 	}
-	else if(empty($password)){
+	else if(empty($pass)){
 		echo "<script type='text/javascript'>alert ('PASSWORD KOSONG !! Silahkan Masukkan Password Dengan Benar');</script>";
 		include "index.php";
 	}
-	else if(empty($repass)){
+	else if(empty($re)){
 		echo "<script type='text/javascript'>alert ('RETYPE PASSWORD KOSONG !!<br/>Silahkan Masukkan Retype Password Sesuai Password');</script>";
+		include "index.php";
+	}
+	else if($pass!=$re){
+		echo"<script type='text/javascript'>alert ('RETYPE PASSWORD SALAH !! Silahkan Masukkan Retype Password Sesuai Dengan Password');</script>";
 		include "index.php";
 	}
 	else{
 		echo "<script type='text/javascript'>alert ('Berhasil Mendaftar');</script>";
 		include "index.php";
-		$query = "INSERT INTO datamember VALUES('".$username."','".$password."')";
+		$query = "INSERT INTO datamember VALUES('".$user."','".$password."')";
 		mysql_query($query);
 		
 		header("location:index.php");
