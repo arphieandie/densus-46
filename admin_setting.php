52ee272c758a03ae693dd366d6d2e9dfdfd41529
DedyChristian <dedytian12@yahoo.com>
2014-01-13 15:45:51 +0700

    Update admin_setting.php

--- admin_setting.php
+++ admin_setting.php
@@ -42,7 +42,7 @@
             <div class="headerfix">
 				<ul class="menu" id="menu">
             		<li>
-						<a href="#" class="menulink"><!--<img src="images/jyjtyj.jpg" width="130px" height="50px" />--></a>
+						<a href="#" class="menulink">NEWBIE</a>
 						<ul>	
                         	<li>
 								<a href="admin_profil.php">Profil</a>
@@ -51,7 +51,7 @@
 								<a href="admin_pesan.php">Message</a>
 							</li>
                             <li>
-								<a href="admin_setting.php">Setting</a>
+								<a href="admin_setting.php">Transaksi</a>
 							</li>
                             <li>
 								<a href="Logout.php">Logout</a>
@@ -67,24 +67,39 @@
        	</div>
     </div>
     <div class="setting">
-    	<div class="judulin"><h2>Setting</h2></div>
+    	<div class="judulin"><h2>Transaksi</h2></div>
         <div class="jarak1"></div>
-    	 <form method="post" name="form" class="formsetting">
-        	Nama Band :<br/>
-      		<input type='text' name='namaBand' /><br />
-       		Basecamp :<br/>
-            <input type='text' name='basecamp' /><br />
-			Aliran :<br/>
-      		<input type='text' name='pukul' /><br />
-       		Personil :<br/>
-            <input type='text' name='ddmmyyyy' /><br />
-			Gambar :<br/>
-            <input type='file' name='foto' class='b' /><br />
-			Keterangan (Maks 20kata):<br/>
-			<textarea name='simpul' cols='100' rows='3'></textarea><br />
-			<input type='submit' value='Tambah' class='b' />
-            <input type="reset" value="Reset" class="b"/>
-        </form>
+        <h3>
+        <style type="text/css">
+		.tabel{
+			border:outset;
+			border-color:#006;
+			background:#DDD;
+			color: #222;
+		}
+		.tabel caption{
+			font-size:24px;
+		}
+		.tabel td{
+			padding:6px;
+			border: solid;
+			border-color: #996;
+		}
+		</style>
+        <table align="left" class="tabel" rules="all">
+        <caption>Kode Transaksi</caption>
+        <tr>
+        <td width="120px">Band Member</td><td width="120px">Lagu</td><td width="120px">Harga</td><td width="120px">Nama</td><td width="120px">Email</td><td width="120px">Bank</td><td width="120px">No Rekening</td><td width="120px">No Telepon</td><td width="120px">Catatan</td><td width="120px">Date</td><td width="120px">Hapus</td>
+        <?php
+				include "db.php";
+				$query = "SELECT * from transaksi order by date desc";
+				$hasil_query = mysql_query($query);
+				while ($status=mysql_fetch_row($hasil_query)) {
+					echo "<tr/><td>$status[0]</td><td>$status[1]</td><td>$status[2] $</td><td>$status[3]</td><td>$status[4]</td><td>$status[5]</td><td>$status[6]</td><td>$status[7]</td><td>$status[8]</td><td>$status[9]</td><td><a href='hapusrek.php?kode=$status[6]'>Hapus</a></td><tr>";
+				}
+			?>
+            </tr>
+            </table></h3>
     </div>
 </body>
 </html>
