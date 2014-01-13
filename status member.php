6ad20e927aff5263911d315cc295b88aa7a668ee
Irfan Arfiandi <arphieandie@gmail.com>
2014-01-13 15:11:52 +0700

    Update status member.php

--- status member.php
+++ status member.php
@@ -3,16 +3,15 @@
 	
 	$komen = $_POST['komen'];
 	$date = date('Y-m-d H:i:s');
-	//$login = $_SESSION['Login'] ;
-	
-	if(empty($komen)){
+	$pengenal= $_POST['id'];
+	if(empty($komen) || $komen == "Status / Kegiatan Yang Akan Datang"){
 		echo "<script type='text/javascript'>alert ('ISI STATUS DENGAN BENAR !!!');</script>";
 		include "login_profil.php";
 	}
 	else{
-		echo "<script type='text/javascript'>alert ('Berhasil');</script>";
-		$query = "INSERT INTO status VALUES('".$komen."','".$date."')";
-		mysql_query($query);
+		//echo "<script type='text/javascript'>alert ('Berhasil');</script";
+		$q = "INSERT into status VALUES('$pengenal','$komen','$date','')"; //memasukkan data ke database
+	   	mysql_query($q);
 		include "login_profil.php";
 	}
 ?>
