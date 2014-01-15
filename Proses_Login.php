310645ddeb6b619ff35ba89a6734e98889bb0f89
adithya maulana <maulanaadithya@gmail.com>
2014-01-15 20:06:12 +0700

    Update Proses_Login.php

--- Proses_Login.php
+++ Proses_Login.php
@@ -8,16 +8,16 @@
 	$username=md5($user);
 	$password=md5($pass);
 	
-	if($username=="d5aedf560b928e289dc4a76d8765bc4e")
+	if($username=="21232f297a57a5a743894a0e4a801fc3")
 		if($password=="0638502d387d0c85e052211359b5a544")
 			echo '<script>alert("Login Admin");window.location.href="admin_profil.php"</script>';
 	
-	$query = "SELECT * FROM datamember WHERE username='".$username."' AND password='".$password."' ";
+	$query = "SELECT * FROM datamember WHERE username='".$user."' AND password='".$password."' ";
 	$hasil_query = mysql_query($query);
 	
 	if(mysql_num_rows($hasil_query)>0){
-		$_SESSION["Login"]=$username;
-		echo '<script>alert("Anda Berhasil Login");window.location.href="login_profil.php"</script>';
+		$_SESSION["Login"]=$user;
+		echo '<script>window.location.href="login_profil.php"</script>';
 	}
 	else{
 		echo '<script>alert("Login Gagal");window.location.href="index.php"</script>';
